b4f99e144583efa5be43000196ee098e61f6f893
Ces1254 <cesare.liguori@chello.at>
2026-03-01 23:32:38 +0100

    Fixed string.json

--- custom_components/ebus_direct/const.py
+++ custom_components/ebus_direct/const.py
@@ -4,14 +4,14 @@ CONF_HOST = "host"
 CONF_PORT = "port"
 CONF_SCAN_INTERVAL = "scan_interval"
 
-CONF_DEVICE_NAME = "device name"
-CONF_DEVICE_MANUFACTURER = "device manufacturer"
-CONF_DEVICE_MODEL = "device model"
+CONF_DEVICE_NAME = "device_name"
+CONF_DEVICE_MANUFACTURER = "device_manufacturer"
+CONF_DEVICE_MODEL = "device_model"
 
 DEFAULT_DEVICE_NAME = "My heating system"
-DEFAULT_DEVICE_MANUFACTURER = "My manufacturer"
-DEFAULT_DEVICE_MODEL = "My furnace model/SN"
+DEFAULT_DEVICE_MANUFACTURER = "My system manufacturer"
+DEFAULT_DEVICE_MODEL = "My system model"
 
 DEFAULT_HOST = "192.168.x.x"
 DEFAULT_PORT = 8888
-DEFAULT_SCAN_INTERVAL = 15  # seconds
+DEFAULT_SCAN_INTERVAL = 30   # seconds
